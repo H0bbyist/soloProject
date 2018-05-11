1437f7b90e42f228aabb69d12c2f97d23e23e368
Jesse Weaver <jesse354@gmail.com>
2018-05-11 14:19:17 -0500

    project ready

--- spotify-app/src/components/MyAlbums.js
+++ spotify-app/src/components/MyAlbums.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import Spotify from 'spotify-web-api-js';
 const spotify = new Spotify();
 
+const style = {
+  width: '25rem',
+  marginTop: '150px'
+
+}
+
 
 class MyAlbums extends Component {
     constructor(props) {
@@ -17,23 +23,53 @@ class MyAlbums extends Component {
         spotify.getMySavedAlbums()
         .then((res) => {
           this.setState({
-            myAlbums: res.items[9].album.images[0].url
+            myAlbums: res.items[2].album.images[0].url
           })
-          console.log(res)
+          
         })
       }
     
       getAnalysis(id) {
         spotify.getAudioFeaturesForTrack(id)
         .then((res) => {
-          console.log(res)
+          
         })
+      }
+
+      componentDidMount() {
+        this.getSavedAlbums()
       }
     
     render() {
+
+
+      const image = this.state.myAlbums;
+
         return(
-        <div>
-            <div>
+          <div className="d-flex align-items-center">
+
+
+            <div className="container">
+              <div className="card" style={style}>
+                <img className="card-img-top" src={image} alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">Arist Name</h5>
+                  <p className="card-text">Album Title</p>
+                  {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                </div>
+              </div>
+            </div>
+
+
+        </div>
+
+
+        )
+    }
+}
+export default MyAlbums
+
+{/* <div>
             <img src={this.state.myAlbums} />
           </div>
           <button onClick={() => this.getSavedAlbums()}>Get My Albums
@@ -41,9 +77,4 @@ class MyAlbums extends Component {
           <div>
             </div>
           <button onClick={() => this.getAnalysis()}>Get Song Analysis
-          </button>
-        </div>
-        )
-    }
-}
-export default MyAlbums
+          </button> */}
